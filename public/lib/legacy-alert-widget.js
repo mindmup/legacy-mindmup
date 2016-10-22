1528b45565af4e88eee06de4cb58ab5f2da8752f
Gojko Adzic <gojko@gojko.com>
2016-10-22 08:56:23 +0200

    legacy widget

--- public/lib/legacy-alert-widget.js
+++ public/lib/legacy-alert-widget.js
@@ -1,6 +1,6 @@
 /*global jQuery*/
 
-jQuery.fn.iosAppStoreAlertWidget = function (propertyStorage, propertyName, tagElement, alertController) {
+jQuery.fn.legacyAlertWidget = function (propertyStorage, propertyName, tagElement, alertController) {
 	'use strict';
 	return jQuery.each(this, function () {
 		var element = jQuery(this),
@@ -9,11 +9,7 @@ jQuery.fn.iosAppStoreAlertWidget = function (propertyStorage, propertyName, tagE
 		if (propertyStorage.getItem(propertyName)) {
 			return;
 		}
-		if (!tagElement.hasClass('ios')) {
-			return;
-		}
-
-		element.find('[data-mm-role="ios-appstore-alert-hide"]').click(function () {
+		element.find('[data-mm-role="legacy-alert-hide"]').click(function () {
 			propertyStorage.setItem(propertyName, true);
 			if (alertId) {
 				alertController.hide(alertId);
